b6a3149c9255e6acf505143f4064d86fd1945ef0
Andrey Smaliy <smaliav@mail.ru>
2021-10-29 23:00:55 +0300

    Moved to Spring Boot

--- src/main/java/ru/smaliav/fitnessbot/Main.java
+++ src/main/java/ru/smaliav/fitnessbot/Main.java
@@ -1,6 +1,8 @@
 package ru.smaliav.fitnessbot;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -8,11 +10,14 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import java.util.Map;
 
 @Slf4j
+@SpringBootApplication
 public class Main {
 
     private static final Map<String, String> env = System.getenv();
 
     public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(new FitnessBot(env.get("BOT_NAME"), env.get("BOT_TOKEN")));
